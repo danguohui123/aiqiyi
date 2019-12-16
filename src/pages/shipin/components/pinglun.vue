5bcb88602465271fde8c73c0c74fbaf2b6236b2f
“e <15138027270@163.com>
2019-12-16 23:56:37 +0800

    ten

--- src/pages/shipin/components/pinglun.vue
+++ src/pages/shipin/components/pinglun.vue
@@ -9,6 +9,27 @@
                 </div>
             </div>
         </div>
+        <div class="sixo">
+            <div class="c-title">
+                <h1 class="hsico">精彩评论</h1>
+                <span class="s-title">(37)</span>
+            </div>
+        </div>
+        <div class="pinglun_center">
+            <div class="pinglun_top">
+                <div class="pinglun_left">
+                    <div class="c-photo"></div>
+                    <div class="info">
+                        <div class="name">123</div>
+                        <span class="time">12月16号</span>
+                    </div>
+                </div>
+                <a class="ping-right">
+                    <i class="iconfont">&#xe676;</i>
+                    <span class="right_text">777</span>
+                </a>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -45,5 +66,57 @@ export default {
     text-align center
     .pinglun_link
      color #0bbe06
-
+ .sixo
+  margin-top 1rem
+  .c-title
+   font-size:1.5rem
+   color:#333
+   height:2rem
+   line-height:2rem
+   margin-left:1rem
+   position:relative
+   .hsico
+    position:absolute
+    font-weight:bold
+   .s-title
+    margin-left 6rem
+ .pinglun_center
+  position relative
+  margin-top 1.5rem
+  padding-bottom .3rem
+  .pinglun_top
+   padding 0 1.2rem .5rem 1.2rem
+   display -webkit-box
+   .pinglun_left
+    display -webkit-box
+    width 85%
+    .c-photo
+     width 3.5rem
+     height 3.5rem
+     border-radius 2rem
+     background-image url(http://img5.iqiyipic.com/image/cartoon/cartoon_5b6164b874d579560d9d4945_1.jpg)
+     background-size cover
+    .info
+     padding 0 1rem
+     .name
+      color #333
+      line-height 2rem
+      font-size 1.5rem
+     .time
+      font-size 1rem
+      color #999
+      line-height 1.3rem  
+   .ping-right
+    display inline-block
+    width 5rem
+    height 3.5rem
+    line-height 3.5rem
+    .iconfont
+     font-size 2.5rem
+    .right_text
+     display inline-block
+     vertical-align top
+     line-height 3.5rem
+     font-size 1rem 
+     
 </style>
