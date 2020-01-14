ae6b1e7d9f0d67f0176f8328b75accb6dd04d919
“e <15138027270@163.com>
2020-01-14 11:28:16 +0800

    1

--- src/pages/home/components/Nav.vue
+++ src/pages/home/components/Nav.vue
@@ -2,8 +2,14 @@
   <div class="topNav">
     <div class="swiper-container">
       <div class="swiper-wrapper">
-        <div class="swiper-slide span-info">精选</div>
-        <div class="swiper-slide">vip会员</div>
+        <div
+          class="swiper-slide"
+          v-for="(item,index) of NavList"
+          :key="item.id"
+          :class="{'span-info':currentIndex == index}"
+          @click="ClickName(index)"
+        >{{item.name}}</div>
+        <!-- <div class="swiper-slide">vip会员</div>
         <div class="swiper-slide">电视剧</div>
         <div class="swiper-slide">电影</div>
         <div class="swiper-slide">儿童</div>
@@ -11,7 +17,7 @@
         <div class="swiper-slide">资讯</div>
         <div class="swiper-slide">直播</div>
         <div class="swiper-slide">选秀</div>
-        <div class="swiper-slide">课程</div>
+        <div class="swiper-slide">课程</div>-->
       </div>
     </div>
     <router-link to="/type">
@@ -26,6 +32,28 @@
 <script>
 export default {
   name: 'HomeNav',
+  data () {
+    return {
+      NavList: [
+        { id: '001', name: '精选' },
+        { id: '002', name: 'vip会员' },
+        { id: '003', name: '电视剧' },
+        { id: '004', name: '电影' },
+        { id: '005', name: '儿童' },
+        { id: '006', name: '动漫' },
+        { id: '007', name: '资讯' },
+        { id: '008', name: '直播' },
+        { id: '009', name: '选秀' },
+        { id: '010', name: '课程' }
+      ],
+      currentIndex: 0
+    }
+  },
+  methods: {
+    ClickName (index) {
+      this.currentIndex = index
+    }
+  },
   mounted () {
     // eslint-disable-next-line no-undef,no-unused-vars
     var mySwiper = new Swiper('.swiper-container', {
